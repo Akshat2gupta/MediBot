c80fffbf17e18c4c9685237bd50ef97c73e7ef22
Akshat Gupta <akshat2gupta@gmail.com>
2025-12-17 22:30:08 +0530

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MediBot",
+    version="0.1.0",
+    author="Akshat Gupta",
+    author_email="akshat2gupta@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
